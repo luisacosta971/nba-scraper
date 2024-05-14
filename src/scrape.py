037e9efedb8b43a3453afad8187e2b41139ef2ab
luisacosta971 <luisacosta971@gmail.com>
2024-05-14 15:52:18 -0500

    csv creation

--- src/scrape.py
+++ src/scrape.py
@@ -1,4 +1,5 @@
 import time
+import csv
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,25 +9,34 @@ url = 'https://www.nba.com/players'
 driver = webdriver.Safari()
 driver.get(url)
 
-time.sleep(3) 
-
-next_button_class = 'Pagination_button__sqGoH'
-driver.find_element(By.XPATH, "//button[@title='Next Page Button']").click()
-time.sleep(3)
-
-html_content = driver.page_source
-soup = BeautifulSoup(html_content, 'html.parser')
-
 data = []
 
-table = soup.find('table', class_='players-list')
-table_body = table.find('tbody')
-rows = table_body.find_all('tr')
-for row in rows:
-    cols = row.find_all('td')
-    cols = [ele.text.strip() for ele in cols]
-    data.append([ele for ele in cols if ele])
+for i in range(12):
+    html_content = driver.page_source
+    soup = BeautifulSoup(html_content, 'html.parser')
     
-print(data)
+    table = soup.find('table', class_='players-list')
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    for row in rows:
+        names = row.find_all('p')
+        names = [ele.text.strip() for ele in names]
+        image = row.find(class_='PlayerImage_image__wH_YX PlayerImage_round__bIjPr')
+        data.append([names[0] + ' ' + names[1], image.get('src')])
+        
+    driver.find_element(By.XPATH, "//button[@title='Next Page Button']").click()
+    time.sleep(2)
 
 driver.quit()
+
+filename = 'nba.csv'
+
+headers = ['Name', 'Image']
+
+with open(filename, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    
+    writer.writerow(headers)
+    writer.writerows(data)
+
+print(f"Data written to {filename}")
