8a8d5c134ba07c0b45f3019e04156a2fea9be1ca
luisacosta971 <luisacosta971@gmail.com>
2024-05-14 01:25:34 -0500

    selenium

--- src/scrape.py
+++ src/scrape.py
@@ -1,14 +1,20 @@
-import requests
+import time
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 url = 'https://www.nba.com/players'
-response = requests.get(url)
 
-if response.status_code == 200:
-    html_content = response.content
-else:
-    print('Failed to retrieve the webpage')
-    
+driver = webdriver.Safari()
+driver.get(url)
+
+time.sleep(3) 
+
+next_button_class = 'Pagination_button__sqGoH'
+driver.find_element(By.XPATH, "//button[@title='Next Page Button']").click()
+time.sleep(3)
+
+html_content = driver.page_source
 soup = BeautifulSoup(html_content, 'html.parser')
 
 data = []
@@ -18,7 +24,9 @@ table_body = table.find('tbody')
 rows = table_body.find_all('tr')
 for row in rows:
     cols = row.find_all('td')
-
+    cols = [ele.text.strip() for ele in cols]
     data.append([ele for ele in cols if ele])
     
 print(data)
+
+driver.quit()
